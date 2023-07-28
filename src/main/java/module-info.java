534a604bd3d4b62f45b5524568d948965d2ad82d
Avishkar_Jagtap <68332049+Avishkar-Sorted-Set@users.noreply.github.com>
2023-07-28 22:33:24 +0530

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.javafxproject.javafxproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.graphics;
+
+    exports com.javafxproject.browser;
+    exports com.javafxproject.chatapp;
+    exports com.javafxproject.quicksort;
+}
